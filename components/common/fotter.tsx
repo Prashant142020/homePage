2bfb372fd9074a7258bbbe4a797d82dac5182741
prashant <prashantkumar142020@gmail.com>
2024-07-10 21:58:39 +0530

    fix : footer layout

--- components/common/fotter.tsx
+++ components/common/fotter.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 const Fotter = () => {
   return (
     <footer className="footer mt-auto">
-      <div className="mt-10 bg-muted py-5">
-        <div className="flex flex-row items-center justify-center gap-2">
+      <div className="mt-10 bg-muted py-3">
+        <div className="flex flex-row items-center justify-center gap-3">
           FOLLOW: <Github />{" "}
           <a
             className="text-blue-500 hover:text-blue-700"
@@ -16,7 +16,7 @@ const Fotter = () => {
             GITHUB
           </a>
         </div>
-        <div className="mt-4 text-center">
+        <div className="mt-1 text-center">
           © 2024 Manu K. Gupta. If you find something useful here,
           <Link
             className="text-blue-500 hover:peer-hover:text-blue-700"
